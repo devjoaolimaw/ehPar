2101404cf315217e0e7fbb28ff78c83f93168235
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-06-08 19:57:04 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int numero = 15;
+bool ehPar = false;
+
+//If ternario
+// ehPar = numero % 2 == 0;
+// Console.WriteLine($"O numero {numero} é " + (ehPar ? "par" : "impar"));
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"O numero {numero} é par");
+}
+else
+{
+    Console.WriteLine($"O numero {numero} é impar");
+}
